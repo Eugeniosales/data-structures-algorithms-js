9527ee7d42ace44219b99ab2f3ca23dcd41219fe
eusiqueira <eugenio.sales@outlook.com>
2024-10-03 08:03:57 -0300

    add 347 top k frequent elements

---
+++ leetCode/sorting/top-k-elements.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {
+  const map = {};
+  // O(n)
+  for (const num of nums) {
+    if (!map[num]) map[num] = 1;
+    else map[num]++;
+  }
+  const arr = [];
+  // O(n)
+  for (const [key, value] of Object.entries(map)) {
+    arr.push({ key, value });
+  }
+  // O(n log n)
+  const sorted = arr.sort((a, b) => b.value - a.value);
+  const ans = [];
+  // O(n)
+  for (let i = 0; i < k; i++) ans.push(Number(sorted[i].key));
+  return ans;
+};
