c58172ccfbb39d195de44f4f807bf40474f35b07
eusiqueira <eugenio.sales@outlook.com>
2024-10-07 23:01:30 -0300

    add 125 valid palindrome

--- leetCode/two-pointers/valid-palindrome.js
+++ leetCode/two-pointers/valid-palindrome.js
@@ -2,14 +2,16 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-  const str = s.replace(/[^0-9a-z]/gi, "").toLowerCase();
-  let left = 0;
-  let right = str.length - 1;
-  while(left < right) {
-      if(str[left] !== str[right]) return false;
-      left++;
-      right--;
+var isPalindrome = function (s) {
+  const isAlpha = function (c) {
+      return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9')
+  }
+
+  let str = s.split('').filter(c => isAlpha(c)).join('').toLowerCase();
+
+  let i = 0, j = str.length - 1;
+  while (i < j) {
+      if (str[i++] !== str[j--]) return false;
   }
   return true;
 };
